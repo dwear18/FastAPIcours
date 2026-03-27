650bd13d4b82586810237c71b54ae55bad2f6c9c
Danil <das8962495@gmail.com>
2026-03-27 21:33:46 +0300

    feat: Добавил схему для валидации отзывов

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field, ConfigDict, EmailStr
 from decimal import Decimal
+from datetime import datetime
 
 class CategoryCreate(BaseModel):
     """
@@ -57,3 +58,26 @@ class User(BaseModel):
 
 class RefreshTokenRequest(BaseModel):
     refresh_token: str
+
+class ReviewCreate(BaseModel):
+    product_id: int = Field(..., description="ID товара, для которого создаётся отзыв")
+    comment: str | None = Field(
+        None,
+        max_length=1000,
+        description="Текст отзыва (необязательно, до 1000 символов)"
+    )
+    grade: int = Field(
+        ...,
+        ge=1,
+        le=5,
+        description="Оценка товара (от 1 до 5)"
+    )
+
+class ReviewResponse(BaseModel):
+    id: int = Field(..., description="Уникальный ID отзыва")
+    user_id: int = Field(..., description="ID пользователя, который оставил отзыв")
+    product_id: int = Field(..., description="ID товара")
+    comment: str | None = Field(None, description="Текст отзыва")
+    comment_date: datetime = Field(..., description="Дата создания отзыва")
+    grade: int = Field(..., description="Оценка товара")
+    is_active: bool = Field(..., description="Активность отзыва")
