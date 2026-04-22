8ab57a1b73e22a77c481e7024439586404f6dc28
Danil <das8962495@gmail.com>
2026-04-22 17:25:55 +0300

    feat: Добавил эндпоинты для работы с корзиной

--- app/main.py
+++ app/main.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI
 
-from app.routers import categories, products, users, reviews
+from app.routers import categories, products, users, reviews, cart
+
 
 app = FastAPI(
     title="FastAPI Интернет-магазин",
     version="0.1.0",
 )
 
+
 app.include_router(categories.router)
 app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(reviews.router)
-
-@app.get("/")
-async def root():
-    """
-    Корневой маршрут, подтверждающий, что API работает.
-    """
-    return {"message": "Добро пожаловать в API интрнет-магазин!"}
+app.include_router(cart.router)
