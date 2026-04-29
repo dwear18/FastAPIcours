46d0f319d84898a3cb7c130e2d69d17377d7d0a9
Danil <das8962495@gmail.com>
2026-04-29 12:16:03 +0300

    feat: Удалил перенаправление на HTTPS и обновил список разрешённых хостов

--- app/main.py
+++ app/main.py
@@ -2,43 +2,34 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 
 from app.routers import categories, products, users, reviews, cart, orders
-
 
 app = FastAPI(
     title="FastAPI Интернет-магазин",
     version="0.1.0",
 )
 
-
-# Сжатие ответов GZip
+# GZip
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
-# Принудительное перенаправление на HTTPS
-app.add_middleware(HTTPSRedirectMiddleware)
-
-# Список разрешённых хостов (укажите свои домены и локальные адреса)
+# Trusted Host
 app.add_middleware(
     TrustedHostMiddleware,
     allowed_hosts=[
         "localhost",
         "127.0.0.1",
-        "localhost:3000",
-        "127.0.0.1:3000",
-        "yourdomain.com",         # ваш боевой домен
-        "*.yourdomain.com",       # все поддомены
+        "yourdomain.com",
+        "*.yourdomain.com",
     ]
 )
 
+# CORS
 origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'null',  # Для тестирования в браузере
-    # Добавьте сюда адреса фронтенда
 ]
 
 app.add_middleware(
@@ -49,7 +40,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+# Routers
 app.include_router(categories.router)
 app.include_router(products.router)
 app.include_router(users.router)
@@ -57,4 +48,5 @@ app.include_router(reviews.router)
 app.include_router(cart.router)
 app.include_router(orders.router)
 
+# Static files
 app.mount("/media", StaticFiles(directory="media"), name="media")
